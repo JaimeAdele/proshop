78b3700f531d122a679463fd38bfe888e26fe158
JaimeAdele <jaime.zuspann@gmail.com>
2025-02-01 13:46:31 -0800

    error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,19 +5,26 @@ import Product from '../models/productModel.js';
 
 // already using /api/products for routes
 
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({}); // empty object to get all products
-  res.json(products);
-}));
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({}); // empty object to get all products
+    res.json(products);
+  })
+);
 
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-  if (product) {
-    return res.json(product);
-  }
-
-  res.status(404).json({ message: 'Product not found' });
-}));
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Resource not found');
+    }
+  })
+);
 
 export default router;
