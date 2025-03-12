beedfd4e680928234849905867dc1e15bd3bdf6a
JaimeAdele <jaimeadele@gmail.com>
2025-03-11 23:58:01 -0700

    create products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 
 // already using /api/products for routes
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
 
 export default router;
