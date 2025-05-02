3aa08edfe95eb7dd0927b88cd956396efa643a9f
JaimeAdele <jaimeadele@gmail.com>
2025-05-02 15:56:40 -0700

    page titles

--- frontend/src/screens/admin/ProductEditScreen.jsx
+++ frontend/src/screens/admin/ProductEditScreen.jsx
@@ -25,7 +25,6 @@ const ProductEditScreen = () => {
   const {
     data: product,
     isLoading,
-    refetch,
     error
   } = useGetProductDetailsQuery(productId);
 
@@ -120,6 +119,7 @@ const ProductEditScreen = () => {
               <Form.Control type='text' placeholder='Enter image url' value={image} onChange={(e) => setImage}></Form.Control>
               <Form.Control type='file' label='Choose file' onChange={uploadFileHandler}></Form.Control>
             </Form.Group>
+            {loadingUpload && <Loader />}
 
             <Form.Group controlId='brand' className='my-2'>
               <Form.Label>Brand</Form.Label>
